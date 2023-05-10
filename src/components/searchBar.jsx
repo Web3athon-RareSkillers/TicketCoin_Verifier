8153ff47bcbdac6d7d73c34f5f5eb0d254fc0893
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-10 21:20:02 +0200

    added searchBar component

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,63 @@
+import React, {useRef} from 'react';
+
+import {Dimensions} from 'react-native';
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+function SearchBar() {
+  const inputRef = useRef(); // Create a reference
+
+  const handlePress = () => {
+    inputRef.current.focus(); // Focus the input when the view is pressed
+  };
+  return (
+    <TouchableOpacity style={styles.outerContainer} onPress={handlePress}>
+      <View style={styles.innerContainer}>
+        <Image
+          style={styles.icon}
+          source={require('../assets/icons/search.png')}
+          resizeMode="contain"
+        />
+        <TextInput
+          ref={inputRef} // Attach the reference to the input
+          style={styles.input}
+          placeholder="Search"
+          placeholderTextColor="#999999"
+        />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    width: SCREEN_WIDTH - 32,
+    borderRadius: 8,
+  },
+  innerContainer: {
+    height: 40,
+    flexDirection: 'row', //
+    alignItems: 'center',
+    borderRadius: 8,
+    borderColor: '#1F1F1F',
+    borderWidth: 1,
+  },
+  icon: {
+    marginHorizontal: 12, // Adds some spacing to the left and right of the icon
+    width: 16,
+    height: 16,
+  },
+  input: {
+    height: '100%',
+    paddingRight: 12,
+    fontFamily: 'NexaLight',
+    color: '#999999', // Text color
+  },
+});
+export default SearchBar;
