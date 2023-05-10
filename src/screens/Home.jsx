c4c086426373cd77191c7bae4c89a675d80ea29d
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-10 21:36:28 +0200

    finished both carousels

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,103 +1,182 @@
-import React, {useCallback, useContext, useEffect, useState} from 'react';
+import React from 'react';
 
-import {
-  Image,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  ImageBackground,
-  View,
-  Text,
-} from 'react-native';
+import {Image, StyleSheet, View, Text} from 'react-native';
 import {Column as Col, Row} from 'react-native-flexbox-grid';
-import {AuthContext} from '../../AuthContext';
-import {useConnection} from '@solana/wallet-adapter-react';
-import {useAuthorization} from '../components/AuthorizationProvider';
+import SearchBar from '../components/searchBar';
+import CarouselComponent from '../components/CarouselComponent';
+import RoundedButton from '../components/roundedButton';
 
-export default function Home({navigation, route}) {
-  const {user, isLoading, login, logout} = useContext(AuthContext);
-
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
-  const {connection} = useConnection();
-  const {selectedAccount} = useAuthorization();
-  const [authorizationInProgress, setAuthorizationInProgress] = useState(false);
-  const [balance, setBalance] = useState(0);
-  const LAMPORTS_PER_SOL = 1000000000;
-  function convertLamportsToSOL(lamports) {
-    return new Intl.NumberFormat(undefined, {maximumFractionDigits: 1}).format(
-      (lamports || 0) / LAMPORTS_PER_SOL,
-    );
-  }
-  const fetchAndUpdateBalance = useCallback(
-    async account => {
-      console.log('Fetching balance for: ' + account.publicKey);
-      const fetchedBalance = await connection.getBalance(account.publicKey);
-      console.log('Balance fetched: ' + fetchedBalance);
-      setBalance(fetchedBalance);
+export default function Home() {
+  const featuredEventsSliderData = [
+    {
+      id: 1,
+      title: 'Ed Sheeran',
+      image: require('../assets/images/ed-sheeran.png'),
     },
-    [connection],
-  );
-  useEffect(() => {
-    if (!selectedAccount) {
-      console.log(error);
-    }
-    console.log(user);
+    {
+      id: 2,
+      title: 'Super Bowl',
+      image: require('../assets/images/super-bowl.png'),
+    },
+    {
+      id: 3,
+      title: 'Beyonce',
+      image: require('../assets/images/beyonce.png'),
+    },
+    {
+      id: 4,
+      title: 'Gretan Van Fleet',
+      image: require('../assets/images/gretan-van-fleet.png'),
+    },
+  ];
 
-    fetchAndUpdateBalance(selectedAccount);
-  }, [fetchAndUpdateBalance, user, selectedAccount]);
+  const allEventsSliderData = [
+    {
+      id: 1,
+      subTitle: 'Ed Sheeran',
+      eventCounter: '123 Events',
+      image: require('../assets/images/ed-sheeran.png'),
+    },
+    {
+      id: 2,
+      subTitle: 'Super Bowl',
+      eventCounter: '1 Event',
+      image: require('../assets/images/super-bowl.png'),
+    },
+    {
+      id: 3,
+      subTitle: 'Beyonce',
+      eventCounter: '109 Events',
+      image: require('../assets/images/beyonce.png'),
+    },
+    {
+      id: 4,
+      subTitle: 'Gretan Van Fleet',
+      eventCounter: '87 Events',
+      image: require('../assets/images/gretan-van-fleet.png'),
+    },
+  ];
   return (
     <>
-      <SafeAreaView>
-        <StatusBar translucent={true} backgroundColor={'transparent'} />
-      </SafeAreaView>
-
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'flex-start',
-          backgroundColor: '#050203',
-        }}>
+      <View style={{flex: 1, backgroundColor: '#0C0C0D'}}>
         <View
           style={{
-            padding: 30,
+            padding: 16,
             flexDirection: 'row',
             flex: 1,
             position: 'absolute',
           }}>
           <Col sm={12}>
-            <Row size={12} style={{marginTop: 20, marginBottom: 20}}>
+            <Row size={12} style={{marginBottom: 32, marginTop: 48}}>
+              <Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <Image source={require('../assets/images/logo_mini.png')} />
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontFamily: 'NexaBold',
+                      paddingLeft: 8,
+                    }}>
+                    Ticketcoin
+                  </Text>
+                </View>
+              </Text>
+            </Row>
+
+            <Row size={12} style={{marginBottom: 16}}>
+              <SearchBar></SearchBar>
+            </Row>
+            <Row size={12} style={{marginBottom: 12}}>
+              <Text
+                style={{
+                  color: 'white',
+                  fontFamily: 'NexaBold',
+                  fontSize: 18,
+                }}>
+                Featured Event
+              </Text>
+            </Row>
+            <Row size={12} style={{marginBottom: 12}}>
+              <CarouselComponent
+                data={featuredEventsSliderData}></CarouselComponent>
+            </Row>
+            <Row size={12} style={{marginBottom: 12}}>
+              <Text
+                style={{
+                  color: 'white',
+                  fontFamily: 'NexaBold',
+                  fontSize: 18,
+                }}>
+                Events
+              </Text>
+            </Row>
+            <Row size={12} style={{marginBottom: 12}}>
               <View
                 style={{
-                  flexDirection: 'column',
-                  marginTop: 80,
-                  alignItems: 'center',
+                  flexDirection: 'row',
+                  flex: 1,
+                  rowGap: 8,
                 }}>
                 <Text
                   style={{
                     color: 'white',
-                    fontFamily: 'NexaBold',
-                    fontSize: 18,
-                    textAlign: 'center',
+                    fontFamily: 'NexaLight',
+                    fontSize: 12,
+                    marginRight: 12,
                   }}>
-                  wallet Address: {selectedAccount && selectedAccount.address}
+                  All
                 </Text>
                 <Text
                   style={{
                     color: 'white',
-                    fontFamily: 'NexaBold',
-                    fontSize: 18,
-                    marginTop: 20,
-                    textAlign: 'center',
+                    fontFamily: 'NexaLight',
+                    fontSize: 12,
+                    marginRight: 12,
                   }}>
-                  {balance !== null
-                    ? `Balance: ${convertLamportsToSOL(balance)} SOL`
-                    : 'Loading balance...'}
+                  Sport
+                </Text>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontFamily: 'NexaLight',
+                    fontSize: 12,
+                    marginRight: 12,
+                  }}>
+                  Music
+                </Text>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontFamily: 'NexaLight',
+                    fontSize: 12,
+                    marginRight: 12,
+                  }}>
+                  Art
+                </Text>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontFamily: 'NexaLight',
+                    fontSize: 12,
+                  }}>
+                  Food
                 </Text>
               </View>
             </Row>
+            <Row size={12} style={{marginBottom: 16}}>
+              <CarouselComponent data={allEventsSliderData}></CarouselComponent>
+            </Row>
           </Col>
+        </View>
+        <View style={styles.floatingContainer}>
+          <RoundedButton
+            onPress={() => navigation.navigate('Verify')}
+            subTitle={'+   Verify Attendee'}
+          />
         </View>
       </View>
     </>
@@ -108,5 +187,11 @@ const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
+  },
+  floatingContainer: {
+    position: 'absolute',
+    width: 160,
+    bottom: 144,
+    right: 16,
   },
 });
