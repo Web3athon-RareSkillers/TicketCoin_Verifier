106f1ed759c0b48703502cb61d837db91ca6bd5f
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-13 17:15:03 +0200

    added config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  files: ['react-native.config.js'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-commonjs': 'off',
+  },
 };
