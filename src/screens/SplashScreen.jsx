7b9ed77d62663813d887c176df1f9b1c6fa658ab
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-13 18:22:20 +0200

    refactored splashscreen, outsourced style to stylesheet

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {
   Image,
   SafeAreaView,
@@ -10,70 +9,34 @@ import {
   Text,
   Dimensions,
 } from 'react-native';
-import {Column as Col, Row} from 'react-native-flexbox-grid';
 import RoundedButton from '../components/roundedButton';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 function SplashScreen({navigation}) {
   return (
-    <>
-      <SafeAreaView>
-        <StatusBar translucent={true} backgroundColor={'transparent'} />
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar translucent={true} backgroundColor={'transparent'} />
 
-      <View style={{flex: 1, justifyContent: 'flex-end'}}>
+      <View style={styles.contentContainer}>
         <ImageBackground
           source={require('../assets/images/home_bg.png')}
           style={styles.backgroundImage}
         />
 
-        <View
-          style={{
-            padding: 16,
-            flexDirection: 'column',
-            flex: 1,
-            position: 'absolute',
-          }}>
-          <View style={{marginBottom: 32}}>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}>
-              <Image source={require('../assets/images/logo_mini.png')} />
-              <Text
-                style={{
-                  color: 'white',
-                  fontFamily: 'NexaBold',
-                  paddingLeft: 8,
-                }}>
-                Ticketcoin
-              </Text>
-            </View>
+        <View style={styles.absoluteContainer}>
+          <View style={styles.logoContainer}>
+            <Image source={require('../assets/images/logo_mini.png')} />
+            <Text style={styles.logoText}>Ticketcoin</Text>
           </View>
 
-          <View style={{marginBottom: 32}}>
-            <Text
-              style={{color: 'white', fontFamily: 'NexaBold', fontSize: 32}}>
-              Secure event tickets using{''}
-              <Text
-                style={{
-                  color: '#7B4ACD',
-                  fontFamily: 'NexaBold',
-                  fontSize: 32,
-                }}>
-                {' '}
-                Blockchain
-              </Text>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>
+              Secure event tickets using{' '}
+              <Text style={styles.highlightedText}>Blockchain</Text>
             </Text>
           </View>
-          <View
-            style={{
-              marginBottom: 48,
-              flex: 1,
-              justifyContent: 'center',
-              width: SCREEN_WIDTH - 32,
-            }}>
+
+          <View style={styles.buttonContainer}>
             <RoundedButton
               onPress={() => navigation.navigate('Onboard')}
               title={'Start Experience    >>'}
@@ -81,14 +44,57 @@ function SplashScreen({navigation}) {
           </View>
         </View>
       </View>
-    </>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
+  },
+  absoluteContainer: {
+    padding: 16,
+    flexDirection: 'column',
+    flex: 1,
+    position: 'absolute',
+  },
+  logoContainer: {
+    marginBottom: 32,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  logoText: {
+    color: 'white',
+    fontFamily: 'NexaBold',
+    paddingLeft: 8,
+  },
+  titleContainer: {
+    marginBottom: 32,
+  },
+  title: {
+    color: 'white',
+    fontFamily: 'NexaBold',
+    fontSize: 32,
+  },
+  highlightedText: {
+    color: '#7B4ACD',
+    fontFamily: 'NexaBold',
+    fontSize: 32,
+  },
+  buttonContainer: {
+    marginBottom: 48,
+    flex: 1,
+    justifyContent: 'center',
+    width: SCREEN_WIDTH - 32,
   },
 });
 
