9593384f4e4342262e35531f51f7f45c144abe8d
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-13 20:31:01 +0200

    created reusable Dropdown & InputField component, added arrow down icon

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+
+const Dropdown = ({options, onSelect}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionSelect = option => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    onSelect(option);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.dropdownButton}
+        onPress={() => setIsOpen(!isOpen)}>
+        <Text style={styles.dropdownButtonText}>
+          {selectedOption || 'Select an option'}
+        </Text>
+        <Image
+          style={styles.arrowDownIcon}
+          source={require('../assets/icons/arrow-down.png')}
+        />
+      </TouchableOpacity>
+
+      {isOpen && (
+        <View style={styles.optionsContainer}>
+          {options.map(option => (
+            <TouchableOpacity
+              key={option}
+              style={styles.optionButton}
+              onPress={() => handleOptionSelect(option)}>
+              <Text style={styles.optionButtonText}>{option}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 3,
+  },
+  dropdownButton: {
+    backgroundColor: 'transparent',
+    borderRadius: 99999,
+    height: 52,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  dropdownButtonText: {
+    color: '#999999',
+    fontFamily: 'NexaBold',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    top: '100%',
+    left: 0,
+    right: 0,
+    backgroundColor: '#0C0C0D',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 8,
+    marginTop: 8,
+  },
+  optionButton: {
+    padding: 8,
+  },
+  arrowDownIcon: {
+    width: 10,
+  },
+  optionButtonText: {
+    color: 'white',
+    fontFamily: 'NexaBold',
+  },
+});
+
+export default Dropdown;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {TextInput, StyleSheet, View} from 'react-native';
+
+const InputField = ({onInputChange, placeholderText}) => {
+  const [text, setText] = useState('');
+
+  const handleTextChange = value => {
+    setText(value);
+    onInputChange(value);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholderText}
+        placeholderTextColor="#999999"
+        value={text}
+        onChangeText={handleTextChange}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+  },
+  input: {
+    backgroundColor: 'transparent',
+    borderRadius: 99999,
+    height: 52,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 8,
+    color: 'white',
+    zIndex: 1,
+  },
+});
+
+export default InputField;
